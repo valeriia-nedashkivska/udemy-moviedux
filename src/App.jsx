f970dec75ca0adb23b35162216b5cf40250a28d0
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-04-13 14:27:38 +0300

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './styles.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -8,9 +9,8 @@ function App() {
       <div className='container'>
         <Header></Header>
       </div>
-      <footer className='footer'>
-        <p className='footer'>Footer content here</p>
-      </footer>
+      
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles.css'
+
+export default function Footer() {
+  
+  const currentYaar = new Date().getFullYear();
+
+  return (
+    <footer className='footer'>
+      <p className='footer-text'>
+      © {currentYaar} Moviedux, All rights reserved.
+      </p>
+    </footer>
+  )
+}
