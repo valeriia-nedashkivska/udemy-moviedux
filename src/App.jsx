62d307c4275b1858fde7d7d1b1d53eddb4a34bcf
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-04-16 23:43:32 +0300

    Add movies in cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './styles.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import MoviesGrid from './components/MoviesGrid'
 
 function App() {
   return (
     <div className='App'>
       <div className='container'>
         <Header></Header>
+        <MoviesGrid></MoviesGrid>
       </div>
       
       <Footer></Footer>
